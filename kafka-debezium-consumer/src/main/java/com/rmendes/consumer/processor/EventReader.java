0b4c018e2097aaca48dcb4c499faead064e0c9b4
Raffael Mendes <rmendes@rmendes.remote.csb>
2020-04-02 15:48:18 -0300

    Trying to convert from string to kafka struct

--- kafka-debezium-consumer/src/main/java/com/rmendes/consumer/processor/EventReader.java
+++ kafka-debezium-consumer/src/main/java/com/rmendes/consumer/processor/EventReader.java
@@ -5,11 +5,16 @@ import javax.enterprise.context.ApplicationScoped;
 import org.apache.kafka.common.protocol.types.Struct;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @ApplicationScoped
 public class EventReader {
 
 	@Incoming("account-events")
-	public void proccess(Struct record) {
-		System.out.println(record.getString("op"));
+	public void proccess(String record) throws JsonMappingException, JsonProcessingException {
+		Struct struct = new ObjectMapper().readValue(record, Struct.class);
+		System.out.println(struct.getString("op"));
 	}
 }
