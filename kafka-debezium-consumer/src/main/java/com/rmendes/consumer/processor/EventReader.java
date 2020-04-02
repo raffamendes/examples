d0761944113636d7e72c8807bf4242615fe96490
Raffael Mendes <rmendes@rmendes.remote.csb>
2020-04-02 15:19:26 -0300

    Adjusting to read a struct from topic

--- kafka-debezium-consumer/src/main/java/com/rmendes/consumer/processor/EventReader.java
+++ kafka-debezium-consumer/src/main/java/com/rmendes/consumer/processor/EventReader.java
@@ -2,13 +2,14 @@ package com.rmendes.consumer.processor;
 
 import javax.enterprise.context.ApplicationScoped;
 
+import org.apache.kafka.common.protocol.types.Struct;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
 @ApplicationScoped
 public class EventReader {
 
 	@Incoming("account-events")
-	public void proccess(String record) {
-		System.out.println(record);
+	public void proccess(Struct record) {
+		System.out.println(record.getString("op"));
 	}
 }
