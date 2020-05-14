437288268e8eb4a87090e5b75c6c646e2fd53c83
Raffael Mendes <rmendes@rmendes.remote.csb>
2020-05-13 23:14:45 -0300

    Creating s2i folder on register-api

---
+++ transaction-api/src/test/java/com/rmendes/transaction/NativeApiIT.java
@@ -0,0 +1,9 @@
+package com.rmendes.transaction;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeApiIT extends ApiTest {
+
+    // Execute the same tests but in native mode.
+}
