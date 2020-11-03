2b2dfcf87b7fe5549d208bcfb6590766f9821349
Raffael Mendes <rmendes@rmendes.remote.csb>
2020-11-03 14:40:33 -0300

    Changing properties on jax filter to Instance class

--- register-api/src/main/java/com/rmendes/register/filter/ThreeScaleFilter.java
+++ register-api/src/main/java/com/rmendes/register/filter/ThreeScaleFilter.java
@@ -2,6 +2,7 @@ package com.rmendes.register.filter;
 
 import java.io.IOException;
 
+import javax.enterprise.inject.Instance;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.Response;
@@ -15,17 +16,17 @@ public class ThreeScaleFilter implements ContainerRequestFilter{
 	static final String API_GATEWAY_HEADER="X-3scale-proxy-secret-token";
 
 	@ConfigProperty(name = "api.gateway.token")
-	String tokenValue;
+	Instance<String> tokenValue;
 
 	@ConfigProperty(name= "api.gateway.enabled")
-	boolean apiGatewayEnabled;
+	Instance<Boolean> apiGatewayEnabled;
 
 	@Override
 	public void filter(ContainerRequestContext requestContext) throws IOException {
-		if(apiGatewayEnabled) {
+		if(apiGatewayEnabled.get().booleanValue()) {
 			if(!requestContext.getHeaders().containsKey(API_GATEWAY_HEADER)) {
 				requestContext.abortWith(Response.status(Status.BAD_GATEWAY).entity("Api Gateway Header not Set").build());
-			}else if(!requestContext.getHeaderString(API_GATEWAY_HEADER).equals(tokenValue)) {
+			}else if(!requestContext.getHeaderString(API_GATEWAY_HEADER).equals(tokenValue.get())) {
 				requestContext.abortWith(Response.status(Status.BAD_GATEWAY).entity("Api Gateway Header Incorrect").build());
 			}
 		}
